6b319c90649eb61dfb0b79aa98f78912ff2e0f6f
SergeiRadchenkov <radchenkovsa@yandex.ru>
2024-07-11 16:29:38 +0300

    lection_06

---
+++ lections/lection_6/task_import_02.py
@@ -0,0 +1,5 @@
+import random
+import random_
+
+print(random.randint(1, 6))
+print(random_.randint(1, 6))
---
+++ lections/lection_6/task_import_03.py
@@ -0,0 +1,4 @@
+import sys, random  # Так импортировать нельзя. Каждый модуль импортируется отдельно
+
+print(sys)
+print(random.randint(1, 6))
